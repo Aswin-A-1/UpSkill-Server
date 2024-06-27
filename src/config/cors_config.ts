2cccfc6b9a2d9e4f53305012d04972c9c0eb8cb9
Aswin A <aswin282002@gmail.com>
2024-06-27 15:25:06 +0530

    change 1 the cors options

--- src/config/cors_config.ts
+++ src/config/cors_config.ts
@@ -1,6 +1,6 @@
 export const corsOption =  {
     origin: 'http://localhost:4200',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'authorization-student', 'authorization-instructor', 'authorization-admin',"Access-Control-Allow-Origin"],
-    credentials: true
+    // methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    // allowedHeaders: ['Content-Type', 'Authorization', 'authorization-student', 'authorization-instructor', 'authorization-admin',"Access-Control-Allow-Origin"],
+    // credentials: true
 }
