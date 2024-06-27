ea40b530f49486c91ecb9f0a3208a35a26db820a
Aswin A <aswin282002@gmail.com>
2024-06-27 17:41:51 +0530

    changes 5 in the server

--- src/config/cors_config.ts
+++ src/config/cors_config.ts
@@ -1,18 +1,18 @@
-// export const corsOption =  {
-//     origin: 'http://localhost:4200',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization', 'authorization-student', 'authorization-instructor', 'authorization-admin',"Access-Control-Allow-Origin"],
-//     credentials: true
-// }
+export const corsOption =  {
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'authorization-student', 'authorization-instructor', 'authorization-admin',"Access-Control-Allow-Origin"],
+    credentials: true
+}
 
 // ./config/cors_config.ts
-import { CorsOptions } from 'cors';
+// import { CorsOptions } from 'cors';
 
-const corsOption: CorsOptions = {
-  origin: 'http://localhost:4200/', // Replace with your frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
+// const corsOption: CorsOptions = {
+//   origin: 'http://localhost:4200/', // Replace with your frontend domain
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// };
 
-export { corsOption };
+// export { corsOption };
