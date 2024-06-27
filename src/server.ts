9bf593d03b446d80d36629f5e31f812c5294abf9
Aswin A <aswin282002@gmail.com>
2024-06-27 15:27:04 +0530

    change 2 the cors options

--- src/server.ts
+++ src/server.ts
@@ -38,8 +38,9 @@ const PORT: any = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(cors(corsOption));
+app.use(cors());
 
+app.options('*', cors());
 // app.get('/', (req: Request, res: Response) => {
 //     res.send('UpSkill')
 // })
