8eceb86797fc241fa4ce3a4858e0d596988eef3f
Aswin A <aswin282002@gmail.com>
2024-07-05 09:57:20 +0530

    Increase size of urlencoded data

--- src/server.ts
+++ src/server.ts
@@ -37,6 +37,7 @@ app.use(session({
 const PORT: any = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors(corsOption));
 
